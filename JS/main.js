960840eebc877930e4e7c9df470dabd7ac1b6442
yousef mostafa said za3balawy <138531500+y-za3balawy@users.noreply.github.com>
2023-07-07 04:04:45 +0300

    ANGORA

---
+++ JS/main.js
@@ -0,0 +1,55 @@
+/* 
+    problems 
+    
+    Sum Of Two Numbers => two numbers
+    
+    Divided of two Numbers => two numbers (x, y), Divided => / 
+
+    the Remain Of dividing Two numbers => two numbers (x, y), The Remain %
+
+    The Minus of Two Numbers => two Numbers (x, y), Minus -
+    
+*/
+
+// Two numbers
+let x = 10;
+let y = 20;
+
+// The Remain Of Two numbers
+let z = y - x;
+
+console.log(z);
+
+
+/*
+
+    The Meaning Of Algorithm
+    problem => solve => To Code
+    
+    
+    How To Design an algorithm
+    Analys The Algoritm
+    Two numbers
+    inputs => initiliazation
+    operation => minus multiply dv
+    output => 
+    how to design an alg
+
+*/
+
+// the area of square => this is a problem
+// The Variables (Height, width)
+// Define The Variables
+let width = 20; // => 1s
+let height = 20; // => 1s
+
+// Operation => area = height * width
+// * => Multiply
+
+// outline the alg operation
+let area = height * width; // => 2s
+
+// Output of this alg
+console.log(area); // 1s
+
+// Time of this alg is 5s
